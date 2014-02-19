1910ad2ee8c3b698bbc0a421ba44db8c509b5af1
OpenShift guest <sascha.friedrich@online.de>
2014-02-19 06:01:24 -0500

    added error-handler

--- scripts/script1.js
+++ scripts/script1.js
@@ -2,7 +2,7 @@ window.onload = getMyLocation;
 
 function getMyLocation(){
     if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(displayLocation);
+        navigator.geolocation.getCurrentPosition(displayLocation,displayError)
     }
     else{
         window.alert("Moep Moep!!!");
@@ -18,4 +18,19 @@ function displayLocation(position){
     var div = document.getElementByID("location");
     div.innerHTML="Lat: " + latitude + " Long: " + longitude; 
     
+}
+
+function displayError(error){
+    var errorTypes ={
+        0 : "Unknown error",
+        1 : "Permission denied by user",
+        2 : "Position is not available",
+        3 : "Request timed out"
+    };
+    
+    var errorMessage = errorTypes[error.code];
+/*    if (error.code == 0 || error.code == 2){
+      errorMessage = errorMessage*/
+    window.alert(""+errorMessage);
+
 }
